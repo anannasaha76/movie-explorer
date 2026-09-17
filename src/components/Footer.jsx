2e3df990d848b0262f50c5db5cb1671198d03ba4
Ananna Saha <anannasaha099@gmail.com>
2026-09-17 22:34:15 +0600

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__inner">
+        <div>
+          <p className="footer__brand">🎬 MovieExplorer</p>
+          <p className="footer__copy">© 2026 MovieExplorer. All rights reserved.</p>
+        </div>
+        <div className="footer__links">
+          <a href="#" target="_blank" rel="noreferrer">GitHub</a>
+          <a href="#" target="_blank" rel="noreferrer">Twitter</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
